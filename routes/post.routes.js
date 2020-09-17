dd023e36aafacc8f06221c38c76cd987f0bdba4e
trangnguyen <nttrang30792@gmail.com>
2020-09-17 21:31:56 +0200

    iteration 2 on going

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -3,8 +3,18 @@ const router = express.Router();
 const User = require('../models/User.model');
 const Post = require('../models/Post.model');
 
-router.get('/post-form', (req, res) => {
-res.render()
+const fileUploader = require('../configs/cloudinary.config');
+
+router.get('/create-post', (req, res) => {
+res.render('post/post-form')
+});
+
+router.post('/create-post', fileUploader.single('image'), (req, res) => {
+  const { title, description } = req.body;
+ 
+  Post.create({ title, description, imageUrl: req.file.path })
+    .then(() => res.redirect('/userProfile'))
+    .catch(error => console.log(`Error while creating a new movie: ${error}`));
 });
 
 
